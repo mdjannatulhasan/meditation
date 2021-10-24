0774ae7e1bda74f22b026edf4e95f96f02951508
Jahidhasan3323 <hasanj635@gmail.com>
2021-10-24 15:56:00 +0600

    home page data's query

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\CourseStub;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return view('frontend.pages.home.home');
+    public function index()
+    {
+        $upcoming_events = Event::whereDate('starting_date', ">=", date('Y-m-d'))
+            ->orderBy('id', 'DESC')
+            ->limit(3)
+            ->get();
+        $indevidual_sessions= CourseStub::with('event')->where('type','indivisual_session')->limit(20)->get();
+        $featured_events= CourseStub::with('event')->where('type','featured_events')->limit(20)->get();
+        dd($featured_events);
+        return view('frontend.pages.home.home', compact($upcoming_events));
     }
 }
