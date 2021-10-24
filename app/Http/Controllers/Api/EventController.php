e468ca67f09a1dc7c56bd8c3ba4df293742e3095
Jahidhasan3323 <hasanj635@gmail.com>
2021-10-24 15:33:12 +0600

    Course Stub check existence event on current type

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\CourseStub;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -15,19 +16,26 @@ class EventController extends Controller
         // NOTE: this is a Backpack helper that parses your form input into an usable array.
         // you still have the original request as `request('form')`
         $form = backpack_form_input();
-
+        $existence = CourseStub::query();
         $options = Event::query();
 
         // if no category has been selected, show no options
         if (! $form['category_id']) {
             return [];
         }
+        if ($form['type']) {
+            $existence = $existence->where('type', $form['type']);
+        }
 
         // if a category has been selected, only show articles in that category
         if ($form['category_id']) {
             $options = $options->where('category_id', $form['category_id']);
+            $existence = $existence->where('category_id', $form['category_id']);
         }
-
+        $existence = $existence->pluck('event_id');
+        if ($existence){
+            $options = $options->whereNotIn('category_id', $existence);
+        }
         if ($search_term) {
             $results = $options->where('title', 'LIKE', '%'.$search_term.'%')->paginate(10);
         } else {
