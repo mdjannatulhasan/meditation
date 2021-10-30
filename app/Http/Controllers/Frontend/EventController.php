18223775d30c9e7f1b3bc4cc72b1133b5d08825d
Foysal6862 <61239971+Foysal6862@users.noreply.github.com>
2021-10-30 20:36:58 +0600

    event

--- app/Http/Controllers/Frontend/EventController.php
+++ app/Http/Controllers/Frontend/EventController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\CourseStub;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Event;
 
 class EventController extends Controller
 {
     public function index(){
-        return view('frontend.pages.events.events');
+        $all_events = CourseStub::with('event')->where('type','featured_events')->limit(20)->get();
+        // dd($all_events);
+        $live_events = Event::whereDate('starting_date', "<", date("Y-m-d"))
+        ->whereDate('ending_date', ">", date("Y-m-d"))
+        ->where('category_id', 1)
+        ->orderBy('id', 'DESC')
+        ->limit(20)->get();
+        $today_events = Event::whereDate('starting_date', "=", date("Y-m-d"))
+        ->where('category_id', 1)
+        ->orderBy('id', 'DESC')
+        ->limit(20)->get();
+        $upcoming_events = Event::whereDate('ending_date', "<", date("Y-m-d"))
+        ->where('category_id', 1)
+        ->orderBy('id', 'DESC')
+        ->limit(20)->get();
+        
+        return view('frontend.pages.events.events', compact('all_events', 'live_events', 'today_events', 'upcoming_events'));
     }
 }
--- resources/views/frontend/pages/events/section/gellary.blade.php
+++ resources/views/frontend/pages/events/section/gellary.blade.php
@@ -7,222 +7,119 @@
             <button type="button" class="ps-700 c00 s23" data-filter=".upcoming-events">Upcoming Events</button>
         </div>
         <div class="row gx-4 gy-5 mix-filter">
+            @foreach ($live_events as $live_event)
             <div class="col-lg-4 col-sm-6 mix live">
                 <div class="featured-events shadow-4 rounded-tt">
                     <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/session1.png" alt="featured events" class="img">
+                        <img src="{{ asset($live_event->image) }}" alt="featured events" class="img">
                         <p class="ps-700 s14 cff fe-live-date tc">Live Now</p>
                     </div>
                     <div class="featured-events-content">
                         <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
+                            <h4 class="ps-700 s18 c00">{{ $live_event->title }}</h4>
                         </div>
                         <div class="featured-events-desc">
                             <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
+                                {{ $live_event->description }}
                             </p>
                             <div class="tr">
                                 <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
                             </div>
                         </div>
                         <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
+                            <a href="{{ url('event-details', $live_event->id) }}" class="primary-btn">Join Now</a>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
+            @foreach ($all_events as $all_event)
             <div class="col-lg-4 col-sm-6 mix live upcoming-events">
                 <div class="featured-events shadow-4 rounded-tt">
                     <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/featured-events2.png" alt="featured events" class="img">
+                        <img src="{{ asset( $all_event->event->image )}}" alt="featured events" class="img">
                         <p class="ps-700 s14 cff fe-live-date fe-date tc">Mon - 07:00 pm</p>
                     </div>
                     <div class="featured-events-content">
                         <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
+                            <h4 class="ps-700 s18 c00">{{ $all_event->event->title }}</h4>
                         </div>
                         <div class="featured-events-desc">
                             <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
+                                {{ $all_event->event->description }}
                             </p>
                             <div class="tr">
                                 <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
                             </div>
                         </div>
                         <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
+                            <a href="{{ url('event-details', $all_event->event->id) }}" class="primary-btn">Join Now</a>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
+            @foreach ($upcoming_events as $upcoming_event)
             <div class="col-lg-4 col-sm-6 mix upcoming-events">
                 <div class="featured-events shadow-4 rounded-tt">
                     <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/featured-events3.png" alt="featured events" class="img">
+                        <img src="{{ asset( $upcoming_event->image )}}" alt="featured events" class="img">
                         <p class="ps-700 s14 cff fe-live-date fe-date tc">Today - 09:00 am</p>
                     </div>
                     <div class="featured-events-content">
                         <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
+                            <h4 class="ps-700 s18 c00">{{ $upcoming_event->title }}</h4>
                         </div>
                         <div class="featured-events-desc">
                             <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
+                                {{ $upcoming_event->description }}
                             </p>
                             <div class="tr">
                                 <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
                             </div>
                         </div>
                         <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
+                            <a href="{{ url('event-details', $upcoming_event->id) }}" class="primary-btn">Join Now</a>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
+            @foreach ($today_events as $today_event)
             <div class="col-lg-4 col-sm-6 mix today-events">
                 <div class="featured-events shadow-4 rounded-tt">
                     <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/session1.png" alt="featured events" class="img">
+                        <img src="{{ asset($today_event->image) }}" alt="featured events" class="img">
                         <p class="ps-700 s14 cff fe-live-date tc">Live Now</p>
                     </div>
                     <div class="featured-events-content">
                         <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
+                            <h4 class="ps-700 s18 c00">{{ $today_event->title }}</h4>
                         </div>
                         <div class="featured-events-desc">
                             <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
+                                {{ $today_event->description }}
                             </p>
                             <div class="tr">
                                 <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
                             </div>
                         </div>
                         <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
+                            <a href="{{ url('event-details', $today_event->id) }}" class="primary-btn">Join Now</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-sm-6 mix live">
-                <div class="featured-events shadow-4 rounded-tt">
-                    <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/featured-events2.png" alt="featured events" class="img">
-                        <p class="ps-700 s14 cff fe-live-date fe-date tc">Mon - 07:00 pm</p>
-                    </div>
-                    <div class="featured-events-content">
-                        <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
-                        </div>
-                        <div class="featured-events-desc">
-                            <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
-                            </p>
-                            <div class="tr">
-                                <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
-                            </div>
-                        </div>
-                        <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mix upcoming-events">
-                <div class="featured-events shadow-4 rounded-tt">
-                    <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/featured-events3.png" alt="featured events" class="img">
-                        <p class="ps-700 s14 cff fe-live-date fe-date tc">Today - 09:00 am</p>
-                    </div>
-                    <div class="featured-events-content">
-                        <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
-                        </div>
-                        <div class="featured-events-desc">
-                            <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
-                            </p>
-                            <div class="tr">
-                                <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
-                            </div>
-                        </div>
-                        <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mix today-events">
-                <div class="featured-events shadow-4 rounded-tt">
-                    <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/session1.png" alt="featured events" class="img">
-                        <p class="ps-700 s14 cff fe-live-date tc">Live Now</p>
-                    </div>
-                    <div class="featured-events-content">
-                        <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
-                        </div>
-                        <div class="featured-events-desc">
-                            <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
-                            </p>
-                            <div class="tr">
-                                <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
-                            </div>
-                        </div>
-                        <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mix live">
-                <div class="featured-events shadow-4 rounded-tt">
-                    <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/featured-events2.png" alt="featured events" class="img">
-                        <p class="ps-700 s14 cff fe-live-date fe-date tc">Mon - 07:00 pm</p>
-                    </div>
-                    <div class="featured-events-content">
-                        <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
-                        </div>
-                        <div class="featured-events-desc">
-                            <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
-                            </p>
-                            <div class="tr">
-                                <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
-                            </div>
-                        </div>
-                        <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mix upcoming-events">
-                <div class="featured-events shadow-4 rounded-tt">
-                    <div class="featured-events-img p-relative">
-                        <img src="{{ asset('') }}asset/frontend/images/featured-events3.png" alt="featured events" class="img">
-                        <p class="ps-700 s14 cff fe-live-date fe-date tc">Today - 09:00 am</p>
-                    </div>
-                    <div class="featured-events-content">
-                        <div class="featured-events-name">
-                            <h4 class="ps-700 s18 c00">Jone smith - Mama Goddess</h4>
-                        </div>
-                        <div class="featured-events-desc">
-                            <p class="ps-400 s16 c51">
-                                There are many variations of passages of Lorem Ipsum available, but the majority.
-                            </p>
-                            <div class="tr">
-                                <a href="javascript:void(0)" class="ps-700 cfd s16">1.7k attending</a>
-                            </div>
-                        </div>
-                        <div class="featured-events">
-                            <a href="javascript:void(0)" class="primary-btn">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
+           
         </div>
     </div>
+    <script src="{{ asset('') }}asset/frontend/js/mixitup.min.js"></script>
+	<script>
+		
+		// MIXITUP 
+		var containerEl = document.querySelector('.mix-filter');
+		var mixer = mixitup(containerEl);
+
+	</script>
 </section>
