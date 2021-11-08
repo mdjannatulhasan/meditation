4312a98d8c9f88131d63f0a266786f16bdf54dd1
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-11-08 21:39:11 +0600

    Bug fixed for links

--- resources/views/frontend/pages/courses/section/banner.blade.php
+++ resources/views/frontend/pages/courses/section/banner.blade.php
@@ -3,7 +3,7 @@
         <div class="same-bg-text tc">
             <h1 class="ps-700 c00 s38">Our {{$title}}</h1>
             <ul class="d-flex align-items-center justify-content-center">
-                <li><a href="index.html" class="ps-700 cff s18">Home</a></li>
+                <li><a href="{{ asset('') }}" class="ps-700 cff s18">Home</a></li>
                 <li class="ps-700 cff s18">|</li>
                 <li><a href="javascript:void(0)" class="ps-700 cb6 s18">Courses </a></li>
             </ul>
