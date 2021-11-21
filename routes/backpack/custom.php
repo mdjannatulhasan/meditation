676ac712bd4cb76805e9461e04bc82644b0599e5
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-11-21 20:11:01 +0600

    1

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,6 +6,7 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
